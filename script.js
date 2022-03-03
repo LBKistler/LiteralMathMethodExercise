14e37d5ff3207906fb80dc7115e9f8717d5f07e6
“lbkistler” <loribethkistler@gmail.com>
2022-03-03 05:57:04 -0600

    uploaded

---
+++ script.js
@@ -0,0 +1,49 @@
+// 1a
+let warmHugs = "Hi, I'm Olaf and I like warm hugs.";
+
+// 1b
+console.log(warmHugs.toUpperCase());
+
+// 1c
+console.log(warmHugs.replace("like", "love"));
+
+// 2a
+let beenImpaled = "Oh, look at that. I've been impaled.";
+
+// 2b
+console.log(beenImpaled.slice(18));
+
+// 3a
+let dotDotDot = "...";
+
+// 3b
+let skullBones = "I don't have a skull" + dotDotDot + "or bones.";
+console.log(skullBones);
+
+// 4
+console.log(Math.PI); 
+
+// 5
+let randomNumber = Math.random();
+randomNumber = Math.ceil(randomNumber);
+console.log(randomNumber);
+
+// B1
+console.log(" Let It Go!".trim().repeat(2).toUpperCase());
+
+
+// B7a
+let reindeers = "Reindeers are better than people.";
+
+// B7b
+console.log(reindeers.replace(/ /g,'_'));
+
+// B8
+console.log(Math.sqrt(2));
+
+// B9
+
+
+
+
+
